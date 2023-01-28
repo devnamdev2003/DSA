886aa0ec40d115f9d9befa395a18a2e160414f18
Dev Namdev <devnamdevcse@gmail.com>
2023-01-28 22:11:34 +0530

    42th day

--- JS/remove_element.js
+++ JS/remove_element.js
@@ -1,8 +1,26 @@
-const a = [3, 2, 2, 3];
-let val = 3;
-const newarr = a.map(function (number, index) {
-      if (number != val) {
-            return index;
-      }
-});
-console.log(newarr);
+var N = 10;
+var A = [];
+for (var i = 1; i <= N; i++) {
+  A.push(i);
+}
+var L = 3;
+var S = 6;
+var subarray = [];
+var sum = 0;
+var found = false;
+for (var i = 0; i < A.length; i++) {
+  subarray.push(A[i]);
+  sum += A[i];
+  if (subarray.length == L) {
+    if (sum == S) {
+      found = true;
+      break;
+    }
+    sum -= subarray.shift();
+  }
+}
+if (found) {
+  console.log('Yes');
+} else {
+  console.log('No');
+}
